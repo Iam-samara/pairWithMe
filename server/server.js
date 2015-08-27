c5fe927116c8a0b083561332e817ad73aa5a64f3
iam-samara <samara.hernandez0@gmail.com>
2015-08-26 19:36:50 -0700

    basic server setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    mongoose = require('mongoose'),
+    app = express(),
+    http = require('http'),
+    bodyParser = require('body-parser'),
+    Schema = moongoose.Schema;
+
+
+mongoose.connect('mongodb://pairwithme:codesmith@ds035593.mongolab.com:35593/pairwithme', function(error){
+  if(error) throw error;
+  else {console.log('connected to DB');}
+})
