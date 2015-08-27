118ba1cf40e539f96ab76ce32991796c8c3f3076
iam-samara <samara.hernandez0@gmail.com>
2015-08-26 19:40:01 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -9,4 +9,6 @@ var express = require('express'),
 mongoose.connect('mongodb://pairwithme:codesmith@ds035593.mongolab.com:35593/pairwithme', function(error){
   if(error) throw error;
   else {console.log('connected to DB');}
-})
+});
+
+console.log('testing if it ask for username');
