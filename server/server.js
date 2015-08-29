c3f563d65a5efd3332301e67914ab722fa1602d4
RyanYocum <RyanCYocum@gmail.com>
2015-08-29 11:01:16 -0700

    seperated server file and started swapping to config

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 var express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
@@ -6,42 +5,37 @@ var express = require('express'),
     OAuth2Strategy = require('passport-oauth').oAuth2Strategy,
     http = require('http'),
     bodyParser = require('body-parser'),
-    Schema = moongoose.Schema;
+    Schema = moongoose.Schema,
+    UserModel = require('./userModel.js'),
+    TagModel = require('./tagModel.js'),
+    ProjectModel = require('./projectModel.js'),
+    config = require('config');
+
 
 /** connection to database */
-mongoose.connect('mongodb://pairwithme:codesmith@ds035593.mongolab.com:35593/pairwithme', function(error){
+mongoose.connect(config.get('databaseLink'), function(error){
   if(error) throw error;
   else {console.log('connected to DB');}
 });
 
 
-
-// users will need more columns
-var userSchema = new Schema({
- _id: {type:String,required: true},
- email:{ type: String, required: true, index: { unique: true } },
- token:{type: String, required: true}
-});
-
-var tagSchema = new Schema({
- tag_id: {type:String,required: true},
- tagName:{ type: String, required: true, index: { unique: true } },
- knowPopularity: {type: Number},
- wantPopularity: {type: Number}
-});
-
-
-
-// projects will also definitely require more columns
-var projectSchema = new Schema({
- project_id: {type:String,required: true},
- projectName:{ type: String, required: true, index: { unique: true } }
-});
-
-
 /** oAuth configurations for this app */
-CLIENT_ID = "114fa33aeb2551ee3084";
-CLIENT_SECRET = "9b98012dbfb5286493ac0b278a1862499e660ae";
+// CLIENT_ID = "114fa33aeb2551ee3084";
+// CLIENT_SECRET = "9b98012dbfb5286493ac0b278a1862499e660ae";
+
+passport.use('GitHub', new OAuth2Strategy({
+    authorizationURL: 'https://github.com/login/oauth/authorize',
+    tokenURL: 'https://www.provider.com/oauth2/token',
+    clientID: '114fa33aeb2551ee3084',
+    clientSecret: '9b98012dbfb5286493ac0b278a1862499e660ae'
+    callbackURL: 'https://www.example.com/auth/provider/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      done(err, user);
+    });
+  }
+));
 
 /**  */
 
