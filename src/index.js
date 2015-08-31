f7a535cc85f5bda5dc8860e5a320a213be71113d
NickTroutwine <Nat88120@ucmo.edu>
2015-08-31 10:45:59 -0700

    fixing file structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var Header = require('./components/all/header.js');
+// var Header = require('./components/all/header.js');
 
 
 React.render(<Header />, document.getElementById('header'));
