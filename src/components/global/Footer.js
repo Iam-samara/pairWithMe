e5b9d186449eb3217bdb0189763e86f0ad1047df
rouzbeh sarrafieh <rouzbeh.sarrafieh@gmail.com>
2015-09-03 17:03:11 -0700

    app/footer/header

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -4,35 +4,28 @@ var Route = Router.Route;
 var Link = Router.Link;
 var Footer = React.createClass({
 	render: function(){
-	return (
-			<nav>
-				<ul>
-					<li>
-						<Link to= "/">Home</Link>
-					</li>
-					<li>
-						<Link to= "guide">Guide</Link>
-					</li>
-					<li>
-						<Link to= "profile">Profile</Link>
-					</li>
-					<li>
-						<Link to= "profileform">Profile Editor</Link>
-					</li>
-					<li>
-						<Link to= "recentprojects">Recent Projects</Link>
-					</li>
-					<li>
-						<Link to= "individualprojects">Individul Projets</Link>
-					</li>
-								<li>
-						<Link to= "search">search</Link>
-					</li>
-					<li>
-						<a href="/auth/github">githubLogin</a>
-					</li>
-				</ul>
-			</nav>
+		return (
+			<div className="navbar clearfix row">
+      <nav>
+        <ul className="nav nav-pills pull-right">
+						<li role="presentation">
+							<Link to="/">Home</Link>
+						</li>
+						<li role="presentation">
+							<Link to="guide">Guide</Link>
+						</li>
+						<li role="presentation">
+							<Link to="profile">Profile</Link>
+						</li>
+						<li role="presentation">
+							<Link to="recentprojects">Recent Projects</Link>
+						</li>
+						<li role="presentation">
+							<Link to="search">search</Link>
+						</li>
+        </ul>
+      </nav>
+    </div>
 		);
 	},
 });
