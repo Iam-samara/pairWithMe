35ae3aaf1aabaf1c8f7ee64a92cf179df1aff208
NickTroutwine <Nat88120@ucmo.edu>
2015-09-05 11:12:28 -0700

    drop boxes implemented

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,9 @@
 var React = require('react');
+var HomeHero = require('./HomeHero')
 var Home = React.createClass({
 	render: function(){
 		return (
-			<div>
-				<img src="https://files.slack.com/files-pri/T063JGQTE-F0A2G1WC9/startup-photos.jpg"/>
-			</div>
+		<div>HomeHero</div>
 		);
 	},
 });
--- src/components/home/HomeHero.js
+++ src/components/home/HomeHero.js
@@ -1,4 +1,12 @@
 var React = require('react');
 var HomeHero = React.createClass({
 
+render: function(){
+	return (
+		<div className="col-xs-8 col-md-10">
+				<img className="img-responsive" src="https://files.slack.com/files-pri/T063JGQTE-F0A2G1WC9/startup-photos.jpg"/>
+		</div>
+		);
+	},
 });
+module.exports = HomeHero;
