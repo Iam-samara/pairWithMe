1ac4adf141c87ee708b77e1a9f2cabc2fcbe3219
NickTroutwine <Nat88120@ucmo.edu>
2015-09-09 16:29:38 -0700

    profile page

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -4,16 +4,10 @@ var UserInfo = React.createClass({
 
 	render: function() {
 		return (
-			<div className="col-sm-offset-4 col-xs-offset-2">
-				<div>
-					<img src="https://avatars3.githubusercontent.com/u/13990032?v=3&s=200"/>
-				</div>
-				<div>
-					<h2>UserName</h2>
-				</div>
-				<div>
-					<p className="col-xs-8 col-sm-2">Email</p>
-					<p className="col-xs-8 col-sm-2 col-sm-offset-4">GitHub</p>
+			<div className="row">
+				<div className="col-xs-12 text-center">
+						<img src="https://avatars3.githubusercontent.com/u/13990032?v=3&s=200" className="img-rounded"/>
+						<p>UserName  |  GitHub</p>
 				</div>
 			</div>
 		);
