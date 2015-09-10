75189b01c061ee9005bf495568568af40acb4df8
NickTroutwine <Nat88120@ucmo.edu>
2015-09-10 12:22:46 -0700

    connected to json object

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -2,15 +2,29 @@ var React = require('react');
 
 
 var UserInfo = React.createClass({
-componentDidMount: function() {
-	
+getInitialState: function() {
+	return {
+		UserName: '',
+		GitHub: '',
+		Pic: '',
+	};
 },
+componentDidMount: function() {
+	var that = this;
+    $.getJSON('/profile1', function(result) {
+    		that.setState({UserName:result.username})
+    		that.setState({Pic:result.githubProfileImage})
+    		that.setState({GitHub:result.githubProfileURL})
+
+      })
+    },
+
 	render: function() {
 		return (
 			<div className="row">
 				<div className="col-xs-12 text-center">
-						<img src="https://avatars1.githubusercontent.com/u/11942769?v=3&s=460?" className="img-rounded"/>
-						<p>UserName  |  GitHub</p>
+					<img src={this.state.Pic}/>
+						<p>{this.state.UserName}  |  {this.state.GitHub}</p>
 				</div>
 			</div>
 		);
