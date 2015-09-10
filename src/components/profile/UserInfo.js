6a55adb6573c0c3deb7da2e0c8188a0a70dfb453
NickTroutwine <Nat88120@ucmo.edu>
2015-09-10 11:19:13 -0700

    starting to get user info

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -2,12 +2,14 @@ var React = require('react');
 
 
 var UserInfo = React.createClass({
-
+componentDidMount: function() {
+	
+},
 	render: function() {
 		return (
 			<div className="row">
 				<div className="col-xs-12 text-center">
-						<img src="https://avatars3.githubusercontent.com/u/13990032?v=3&s=200" className="img-rounded"/>
+						<img src="https://avatars1.githubusercontent.com/u/11942769?v=3&s=460?" className="img-rounded"/>
 						<p>UserName  |  GitHub</p>
 				</div>
 			</div>
