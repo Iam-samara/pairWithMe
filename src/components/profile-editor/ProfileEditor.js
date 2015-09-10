769fe413d3016597e1a3cfa57c1d86d739ec967a
NickTroutwine <Nat88120@ucmo.edu>
2015-09-09 19:28:07 -0700

    profile button

--- src/components/profile-editor/ProfileEditor.js
+++ src/components/profile-editor/ProfileEditor.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var Router= require('react-router');
+var Route = Router.Route;
+var Link = Router.Link;
 var UserInfo = require("../profile/UserInfo.js");
 var ProfileForm = require("./ProfileForm");
 var ProfileEditor = React.createClass({
@@ -7,6 +10,11 @@ var ProfileEditor = React.createClass({
 		return (
 			<div>
 				<UserInfo/>
+				<div className="row">
+					<div className="col-xs-12 text-center">
+						<Link to="profile" className="edit-profile-btn"> Profile </Link>
+					</div>
+				</div>
 				<ProfileForm/>
 			</div>
 		);
--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -1,5 +1,6 @@
 var React = require('react');
 
+
 var UserInfo = React.createClass({
 
 	render: function() {
