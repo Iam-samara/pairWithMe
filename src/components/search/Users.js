eea87f1e9e71d44fd2dcc751731373f3a70da5c7
NickTroutwine <Nat88120@ucmo.edu>
2015-09-16 10:35:44 -0700

    some styling

--- src/components/search/Users.js
+++ src/components/search/Users.js
@@ -13,17 +13,17 @@ var Users = React.createClass({
 	},
 	render: function() {
 		return (
-			<div className="col-xs-12 col-sm-3 col-lg-4">
-				<div>
+			<div className="col-xs-12 col-sm-3 col-lg-4 bordered">
+				<div className="text-center">
 					<img src={this.props.picture} className="img-rounded profile-pic"/>
 				</div>
-				<div>
+				<div className="text-center">
 					<p>{this.props.name}</p>
 				</div>
-				<div>
+				<div className="text-center">
 					<a href={this.props.email}>Email</a>
 				</div>
-				<div>
+				<div className="text-center">
 					<a href={this.props.github}>GitHub</a>
 				</div>
 			</div>
