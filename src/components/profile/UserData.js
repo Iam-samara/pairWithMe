0412bacbd9e5176c6ef1f992b4d3f0fc57981325
NickTroutwine <Nat88120@ucmo.edu>
2015-09-15 18:02:22 -0700

    profile projects

--- src/components/profile/UserData.js
+++ src/components/profile/UserData.js
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var UserProjects = require('./UserProjects.js')
 var UserData = React.createClass({
 	getInitialState: function() {
 		return {
@@ -7,7 +7,8 @@ var UserData = React.createClass({
 			wantTags: [],
 			teacher: '',
 			collab: '',
-			student: ''
+			student: '',
+			projects: []
 		};
 	},
 	componentDidMount: function() {
@@ -21,46 +22,54 @@ var UserData = React.createClass({
 			if(data.student) {
 				var learn = "Student"
 			}
-
 			var known = data.known.map(function (element, index) {
 				return (element.tagName +", ");
 			});
 			var wanted = data.want.map( function (element,index) {
 				return (element.tagName +", ");
 			});
+			var ownedproject = data.ownedproject;
+		console.log(ownedproject);
 			this.setState({
 				knownTags: known,
 				wantTags: wanted,
 				teacher: teach,
 				collab: collaborator,
-				student: learn
+				student: learn,
+				projects: ownedproject
 			});
 		}.bind(this));
 	},
 	render: function() {
+		var userProj = this.state.projects.map(function(element, index){
+			return(<UserProjects title={element.projectName} tools={element.tools} route={element.id} key={index}/>)
+		})
 		return (
-			<div className="bordered">
-				<div className="row">
-					<div className="col-xs-12 col-sm-4 text-center">
-						<strong>{this.state.teacher}</strong>
+			<div>
+				<div className="bordered">
+					<div className="row">
+						<div className="col-xs-12 col-sm-4 text-center">
+							<strong>{this.state.teacher}</strong>
+						</div>
+						<div className="col-xs-12 col-sm-4 text-center">
+							<strong>{this.state.collab}</strong>
+						</div>
+						<div className="col-xs-12 col-sm-4 text-center">
+							<strong>{this.state.student}</strong>
+						</div>
+					</div>				
+					<div className="row">
+						<div className="col-xs-10 col-xs-offset-1 bordered">
+							<strong>Skills Have: </strong>{this.state.knownTags}
+						</div>
 					</div>
-					<div className="col-xs-12 col-sm-4 text-center">
-						<strong>{this.state.collab}</strong>
-					</div>
-					<div className="col-xs-12 col-sm-4 text-center">
-						<strong>{this.state.student}</strong>
-					</div>
-				</div>				
-				<div className="row">
-					<div className="col-xs-10 col-xs-offset-1 bordered">
-						<strong>Skills Have: </strong>{this.state.knownTags}
+					<div className="row">
+						<div className="col-xs-10 col-xs-offset-1 bordered">
+							<strong>Skills Wanted: </strong>{this.state.wantTags}
+						</div>
 					</div>
 				</div>
-				<div className="row">
-					<div className="col-xs-10 col-xs-offset-1 bordered">
-						<strong>Skills Wanted: </strong>{this.state.wantTags}
-					</div>
-				</div>
+				{userProj}
 			</div>
 		);
 	}
--- src/components/profile/UserProjects.js
+++ src/components/profile/UserProjects.js
@@ -1,4 +1,29 @@
 var React = require('react');
 var UserProjects = React.createClass({
-
+	getInitialState: function() {
+		return {
+			title: '',
+			tools: '',
+			route: ''
+		};
+	},
+	handle: function(){
+		 window.location.pathname = '/project/'+ this.props.route
+	},
+	render: function() {
+		return (
+			<div className="col-xs-12 col-sm-4 bordered">
+				<div className="row">
+					<p className="text-center">{this.props.title}</p>
+				</div>
+				<div className="row">
+					<p className="text-center">{this.props.tools}</p>
+				</div>
+				<div className="row center">
+					<button className="center" onClick={this.handle}>Project Path</button>
+				</div>
+			</div>
+		);
+	}
 });
+module.exports = UserProjects;
