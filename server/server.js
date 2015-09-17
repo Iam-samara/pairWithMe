272a22670d069f3072d2c4b83903967676af7e83
Iam-samara <Samara.hernandez0@gmail.com>
2015-09-16 17:33:37 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  morgan = require('morgan');
+  morgan = require('morgan'),
   Sequelize = require('sequelize'),
   config = require('config'),
   http = require('http'),
